b2d5598bb6d2ff75234e1b3988bc35c3e9d731b0
Gustavo Andre <gugamathiaz12@gmail.com>
2024-09-28 21:06:11 -0300

    Lidando com condições

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,36 @@
 function calculate(): void {
-  //const nome: string = "Gustavo";
-  const altura: number = 1.72;
-  const peso: number = 70;
+  const nome: string = "Gustavo";
+  const altura: number = 1.80;
+  const peso: number = 84;
 
   const IMC: number = (peso / (altura * 2));
 
-  display(IMC);
+  display(IMC, nome);
 }
 
-function display(IMC: number): void {
+function display(IMC: number, nome: string): void {
   switch (true) {
     case IMC < 18.5:
-      console.log("abaixo do peso");
+      console.log(`${nome} você está abaixo do peso`);
       break;
     case IMC < 25:
-      console.log("com peso ideal.");
+      console.log(`${nome} você está com peso ideal.`);
       break;
     case IMC < 30:
-      console.log("levemente acima do peso")
+      console.log(`${nome} você está levemente acima do peso`)
       break;
     case IMC < 35:
-      console.log("com obesidade grau I.");
+      console.log(`${nome} você está com obesidade grau I.`);
       break;
     case IMC < 40:
-      console.log("com obesidade grau II.");
+      console.log(`${nome} você está com obesidade grau II.`);
       break;
     case IMC < 50:
-      console.log("com obesidade grau III.");
+      console.log(`${nome} você está com obesidade grau III.`);
       break;
     default:
       break;
   }
 }
-
 
 calculate();
